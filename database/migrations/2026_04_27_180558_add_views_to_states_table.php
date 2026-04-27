d0440cf68cfe77ff61304eaf9fef0458e8123615
Hasanul <rubelcseku11@gmail.com>
2026-04-28 00:09:53 +0600

    admin design partially

--- database/migrations/2026_04_27_180558_add_views_to_states_table.php
+++ database/migrations/2026_04_27_180558_add_views_to_states_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('states', function (Blueprint $table) {
@@ -20,16 +17,6 @@ return new class extends Migration
     {
         Schema::table('states', function (Blueprint $table) {
             $table->dropColumn('views');
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('states', function (Blueprint $table) {
-            //
         });
     }
 };
